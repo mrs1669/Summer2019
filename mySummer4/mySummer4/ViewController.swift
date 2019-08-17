b97defe08d922912082d7b87317afe5c9310e3b1
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-08-17 23:30:25 +0900

    make mysummer4

---
+++ mySummer4/mySummer4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mySummer4
+//
+//  Created by Takumi Muraishi on 2019/08/17.
+//  Copyright © 2019 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
